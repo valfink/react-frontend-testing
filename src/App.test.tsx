40237da438d760fdd61eee450621ed5f996a23cc
valfink <117605562+valfink@users.noreply.github.com>
2023-05-22 14:49:48 +0200

    Add increase test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {cleanup, render, screen} from '@testing-library/react';
+import {cleanup, fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 
 afterEach(cleanup);
@@ -10,5 +10,13 @@ describe("The counter", () => {
         const counter = screen.getByText(/[0-9]+/i);
         expect(counter).toBeInTheDocument();
         expect(counter.textContent).toBe('Counter: 0');
+    });
+
+    it('increments on button click', () => {
+        render(<App/>);
+        const counter = screen.getByText(/[0-9]+/i);
+        expect(counter).toBeInTheDocument();
+        fireEvent.click(screen.getByText(/INCREMENT/));
+        expect(counter.textContent).toBe('Counter: 1');
     });
 });
